947b8d83a7ec19dc4fa61f9d46b53a25ded9f746
Robin Miller <robin@tenjin.ca>
2022-10-07 16:24:28 -0600

    Added docs on scope

--- lib/dirt/envelope/scope.rb
+++ lib/dirt/envelope/scope.rb
@@ -2,6 +2,7 @@
 
 module Dirt
    module Envelope
+      # A set of configurations
       class Scope
          def initialize(data = nil)
             @data = convert(data)
@@ -11,13 +12,22 @@ module Dirt
             freeze
          end
 
+         # Retrieve the value for the given key
+         #
+         # @param [symbol] key
+         # @raise KeyError if no such key exists.
+         # @see #override
          def fetch(key)
-            @data_override.fetch(key.to_sym, @data.fetch(key.to_sym))
+            @data_override.fetch(key.downcase.to_sym, @data.fetch(key.to_sym))
          end
 
          alias / fetch
          alias [] fetch
 
+         # Overrides the given set of key-value pairs. This is intended to only be used in testing environments,
+         # where you may need contextual adjustments to suit the test situation.
+         #
+         # @param [Hash] pairs the hash of pairs to override.
          def override(pairs)
             @data_override.merge!(pairs)
             @data_override.freeze
